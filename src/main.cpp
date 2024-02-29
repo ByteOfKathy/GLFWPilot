a18ff51b801bf43d9b39641a13e8a719524a4c48
kathy <katherinechan4@gmail.com>
2024-02-29 14:05:17 -0500

    remove build folder and fix seg fault

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
-#include "include/glad/glad.h"
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include "Sphere.h"
 
 // Vertex Shader source code
 const char* vertexShaderSource = "#version 330 core\n"
@@ -18,37 +19,45 @@ const char* fragmentShaderSource = "#version 330 core\n"
 
 
 int main() {
+    // glfw initializations
     if (!glfwInit()) {
         return -1;
     }
-
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
-    GLfloat vertices[] = {
-        0.0f, 0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f
-    };
-
+    // window creation
     GLFWwindow* window = glfwCreateWindow(640, 480, "la hee", NULL, NULL);
-    gladLoadGL();
 
-    glClearColor(0.75f, 0.13f, 0.17f, 1.0f);
-    glViewport(0, 0, 800, 800);
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-
+    // shaders
+    // GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    
+    // ensure the window is created
     if (!window) {
         glfwTerminate();
         return -1;
     }
 
+    // set camera and context
+    // glViewport(0, 0, 800, 800);
     glfwMakeContextCurrent(window);
 
+    // math to draw a sphere using sections
+    Sphere sphere(1.0f, 32, 32);
+
+    sphere.printSelf();
+
     while (!glfwWindowShouldClose(window)) {
+        // background color
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+
+        sphere.draw();
+
+        // swap buffers and poll events
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
